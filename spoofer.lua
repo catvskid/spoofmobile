aa6a61a15a61db0c83249dc28f74ace667c36a4d
random <66913721+catvskid@users.noreply.github.com>
2021-08-08 12:12:03 -0300

    Create spoofer.lua

---
+++ spoofer.lua
@@ -0,0 +1,21 @@
+--- ///
+
+call(function() -- for games that ban on error
+if not getgenv().MTAPIMutex then loadstring(game:HttpGet("https://pastebin.com/raw/UwFCVrhS", true))() end
+local UserInputService = game:GetService("UserInputService")
+
+
+UserInputService:AddGetHook("TouchEnabled", function(t,...)
+    if getcallingscript():IsDescendantOf(game.CoreGui) then return false end
+    return true
+end)
+UserInputService:AddGetHook("MouseEnabled",function(t,...)
+    if getcallingscript():IsDescendantOf(game.CoreGui) then return true end
+    return false
+end)
+UserInputService:AddGetHook("KeyboardEnabled", function(t,...)
+    if getcallingscript():IsDescendantOf(game.CoreGui) then return true end
+    return false
+end)
+
+end)
